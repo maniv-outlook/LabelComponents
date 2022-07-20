a8a7b2afbcd3026830fda707fdd7772a2cdaae92
Mani Venkiteswaran <manivenkit@hotmail.com>
2022-07-20 21:29:45 +0530

    commit

---
+++ Areas/MyFeature/Pages/UploaderControl.razor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabelComponents.Areas.MyFeature.Pages
+{
+    public partial class UploaderControl
+    {
+        [Parameter]
+        public string ID
+        {
+            get;
+            set;
+        }
+
+        [Parameter]
+        public string Placeholder
+        {
+            get;
+            set;
+        }
+
+        [Parameter]
+        public bool Multiple
+        {
+            get;
+            set;
+        }
+
+        [Parameter]
+        public bool AutoUpload
+        {
+            get;
+            set;
+        }
+
+        [Parameter]
+        public bool SequentialUpload
+        {
+            get;
+            set;
+        }
+
+        [Parameter]
+        public EventCallback<UploadChangeEventArgs> OnFileUpload
+        {
+            get;
+            set;
+        }
+    }
+}
